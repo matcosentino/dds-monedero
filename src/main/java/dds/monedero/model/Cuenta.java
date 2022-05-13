6c03c0e9e181d08b770c2408afee39f9899a2648
Matias Cosentino <matcosentino@frba.utn.edu.ar>
2022-05-13 01:07:36 -0300

    Long Method: el metodo poner se descompone en varias partes

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -27,14 +27,8 @@ public class Cuenta {
   }
 
   public void poner(double cuanto) {
-    if (cuanto <= 0) {
-      throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
-    }
-
-    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
-      throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
-    }
-
+    validarMontoPositivo(cuanto);
+    validarCantidadDepositosDiarios();
     new Movimiento(LocalDate.now(), cuanto, true).agregateA(this);
   }
 
@@ -78,4 +72,15 @@ public class Cuenta {
     this.saldo = saldo;
   }
 
+  private void validarMontoPositivo(double cuanto) {
+    if (cuanto <= 0) {
+      throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
+    }
+  }
+
+  private void validarCantidadDepositosDiarios() {
+    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
+      throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
+    }
+  }
 }
